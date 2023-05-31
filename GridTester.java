2e22d1a6cb12acc4e0639a9b979133e668e92cf9
weisa592 <135157078+weisa592@users.noreply.github.com>
2023-05-31 11:36:22 -0700

    Create GridTester.java

---
+++ GridTester.java
@@ -0,0 +1,10 @@
+public class GridTester extends ConsoleProgram
+{
+    public void run()
+    {
+        Grid grid = new Grid();
+        grid.printStatus();
+        grid.printShips();
+
+    }
+}
