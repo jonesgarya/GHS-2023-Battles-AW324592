cf777e93f37313e0d90eccd0618273f1d0d1c2f3
weisa592 <135157078+weisa592@users.noreply.github.com>
2023-06-02 11:10:29 -0700

    Final update to Battleship.java

--- Battleship.java
+++ Battleship.java
@@ -2,18 +2,30 @@ public class Battleship extends ConsoleProgram
 {
     public void run()
     {
-        Player playerOne = new Player("One");
-        Player playerTwo = new Player("Two");
+        Player playerOne = new Player("Player");
+        Player playerTwo = new Player("Bot");
         // Start here! This class should interact with the user
         // to play the game of Battleship
         playerOne.askShips(playerTwo);
-        playerTwo.askShips(playerOne);
-        // You only need to allow for the user to set up each player's
-        // ships and for each player to make a guess on the other player's grid
-        // Don't worry about finishing the whole game yet.
-        askForGuess(playerOne, playerTwo);
-        // You will probably need to make additions to the Player class to
-        // allow for this setting up and guessing
+        playerTwo.autoPutShips(playerOne);
+        
+        while(true)
+        {
+            askForGuess(playerOne, playerTwo);
+            if(playerTwo.noLivesCheck())
+            {
+                winner(playerOne);
+                break;
+            }
+            botGuess(playerTwo, playerOne);
+            if(playerOne.noLivesCheck())
+            {
+                winner(playerOne);
+                break;
+            }
+            
+        }
+        
     }
     
     public void askForGuess(Player guesser, Player other)
@@ -36,20 +48,24 @@ public class Battleship extends ConsoleProgram
                         int convertedGCol = guessCol - 1; //convert guessCol to fit the grid build
                         if(guessRowInt > -1 && guessRowInt < 10 && convertedGCol > -1 && convertedGCol < 10) //make sure they are between 0 and 9 (also checks if letter between A-J)
                         {
-                            //at this point, guessRow had to be A-J and guessCol between 1-10.
-                            //now we can do a guess and see if it is a hit or miss
                             
-                            if(other.checkForHit(guessRowInt, convertedGCol))
-                            {
-                                guesser.markHit(guessRowInt, convertedGCol);
-                                println("It was a HIT!");
+                            if(guesser.alreadyGuess(guessRowInt, convertedGCol) != true)
+                            {       //at this point, guessRow had to be A-J and guessCol between 1-10.
+                                    //now we can do a guess and see if it is a hit or miss
+                                if(other.checkForHit(guessRowInt, convertedGCol))
+                                {
+                                    guesser.markHit(guessRowInt, convertedGCol);
+                                    println("It was a HIT!");
+                                    other.loseALife();
+                                }
+                                else
+                                {
+                                    guesser.markMiss(guessRowInt, convertedGCol);
+                                    println("It was a MISS!");
+                                }
+                                break;
                             }
-                            else
-                            {
-                                guesser.markMiss(guessRowInt, convertedGCol);
-                                println("It was a MISS!");
-                            }
-                            break;
+                            
                         }
                     }
                 }
@@ -62,4 +78,51 @@ public class Battleship extends ConsoleProgram
         guesser.printMyGuesses();
         String useless2 = readLine("Press Enter to continue.");
     }
+    
+    public void botGuess(Player guesser, Player other)
+    {
+        int attemptGuess = 0;
+        String name = guesser.getName();
+        println("Your turn to guess, " + name + ". ");
+        while(true) //repeat until legal
+        {
+            attemptGuess++;
+            int guessRow = Randomizer.nextInt(0, 9);
+            int guessCol = Randomizer.nextInt(0, 9);
+            
+            if(guessRow > -1 && guessRow < 10 && guessCol > -1 && guessCol < 10) //make sure they are between 0 and 9 (also checks if letter between A-J)
+            {
+                //at this point, guessRow had to be A-J and guessCol between 1-10.
+                //now we can do a guess and see if it is a hit or miss
+                if(guesser.alreadyGuess(guessRow, guessCol) != true)
+                {
+                    if(other.checkForHit(guessRow, guessCol))
+                    {
+                        guesser.markHit(guessRow, guessCol);
+                        println("Computer HIT after " + attemptGuess + " attempts.");
+                        other.loseALife();
+                    }
+                    else
+                    {
+                        guesser.markMiss(guessRow, guessCol);
+                        println("Computer MISS after " + attemptGuess + " attempts.");
+                    }
+                    break; 
+                }
+                
+            }
+        }
+        
+        guesser.printMyGuesses();
+        String useless2 = readLine("Press Enter to continue.");
+        
+    }    
+    
+    public void winner(Player winner)
+    {
+        String name = winner.getName();
+        println("The winner of battleship is, " + name.toUpperCase() + "!");
+    }
+    
+    
 }
