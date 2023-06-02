2e79038f1f589de0b7a88a92b4b85965bd11a7ac
weisa592 <135157078+weisa592@users.noreply.github.com>
2023-06-02 11:11:55 -0700

    Final update to Player.java

--- Player.java
+++ Player.java
@@ -5,6 +5,7 @@ public class Player extends ConsoleProgram
     private Grid playerGrid;
     private Grid oppGrid;
     private String name;
+    private int lives;
     public static final int HORIZONTAL = 0;
     public static final int VERTICAL = 1;
     private static final int[] SHIP_LENGTHS = {2, 3, 3, 4, 5};
@@ -13,6 +14,7 @@ public class Player extends ConsoleProgram
         this.name = name;
         playerGrid = new Grid();
         oppGrid = new Grid();
+        lives = 17;
     }
     
     public String getName()
@@ -46,6 +48,13 @@ public class Player extends ConsoleProgram
         }
     }
     
+    public boolean alreadyGuess(int r, int c)
+    {
+        
+        return oppGrid.alreadyGuessed(r, c);
+        
+    }
+    
     public void markHit(int r, int c)
     {
         oppGrid.markHit(r, c);
@@ -54,6 +63,29 @@ public class Player extends ConsoleProgram
     public void markMiss(int r, int c)
     {
         oppGrid.markMiss(r, c);
+    }
+    
+    public void loseALife()
+    {
+        lives--;
+        
+    }
+    
+    public boolean noLivesCheck()
+    {
+        if(lives > 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    
+    public int getLives()
+    {
+        return lives;
     }
     
     public void askShips(Player p2)
@@ -114,7 +146,7 @@ public class Player extends ConsoleProgram
                                 {
                                     for (int j = 0; j < SHIP_LENGTHS[i]; j++)
                                     {
-                                        if(playerGrid.hasShip(pRowInt + j, convertedCol))//same as above comment
+                                        if(playerGrid.hasShip(pRowInt, convertedCol + j))//same as above comment
                                         {
                                             fault = 1;
                                         }
@@ -143,10 +175,78 @@ public class Player extends ConsoleProgram
         String useless2 = readLine("Press Enter to continue.");
     }
     
-    public void autoPutShips()
+    public void autoPutShips(Player p2)
     {
-        //soon
+        int attempts = 0;
+        String name = getName();
+        println("Your turn to place ships, " + name + ". ");
+        for (int i = 0; i < SHIP_LENGTHS.length; i++) //repeat code for each Ship
+        {
+            while(true) //repeat until legal
+            {
+                attempts++;
+                int pRow = Randomizer.nextInt(0, 9);
+                int pCol = Randomizer.nextInt(0, 9);
+                int directionInt = Randomizer.nextInt(0, 1);
+                int direction = 2; //if it stays as 2 theres an error
+                if(directionInt == 0) //horizontal
+                {
+                    if(pCol + SHIP_LENGTHS[i] < 10) //make sure it fits
+                    {
+                        direction = 0;
+                    }
+                }
+                if(directionInt == 1) //vertical
+                {
+                    if(pRow + SHIP_LENGTHS[i] < 10) //make sure it fits
+                    {
+                        direction = 1;
+                    }
+                }
+                if(direction != 2) 
+                {
+                    int fault = 0; //changes to 1 if the ship intersects with another
+                    if(direction == 1)
+                    {
+                        for (int j = 0; j < SHIP_LENGTHS[i]; j++)
+                        {
+                        
+                            if(playerGrid.hasShip(pRow + j, pCol))//checks if theres is already a ship
+                            {
+                                fault = 1;
+                            }
+                        }
+                        
+                    }
+                    else
+                    {
+                        for (int j = 0; j < SHIP_LENGTHS[i]; j++)
+                        {
+                            if(playerGrid.hasShip(pRow, pCol + j))//same as above comment
+                            {
+                                fault = 1;
+                            }
+                        }
+                    }
+                    if(fault == 0)
+                    { //at this point, pRow had to be A-J, pCol between 1-10, and pDir starting with H or V.
+                    //now we can place the ships
+                        println("Successfuly placed Ship with length " + SHIP_LENGTHS[i] + " in a safe location after " + attempts + " attempts.");
+                        chooseShipLocation(new Ship(SHIP_LENGTHS[i]), pRow, pCol, direction); //puts ship with the info
+                        p2.chooseShipOppLocation(new Ship(SHIP_LENGTHS[i]), pRow, pCol, direction); //puts ship on other players oppGrid.
+                        attempts = 0;
+                        break;
+                    }
+                }
+                
+                
+            }
+            
+        }
+        
+        String useless2 = readLine("Press Enter to continue.");
     }
+    
     
     public void printMyShips()
     {
